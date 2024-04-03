834a533863c9e30dd0e605a77a1cbcfc04859f25
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-04-03 20:27:48 -0300

    refactore: remove on devise registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,3 @@
 class User < ApplicationRecord
-  
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
 end
